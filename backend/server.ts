5c7f0804f00c0c5ae40670d90bba2629b11a449c
Javier Gonzalez <javgon2306@gmail.com>
2024-10-05 12:42:02 -0300

    fix tsconfig

--- backend/server.ts
+++ backend/server.ts
@@ -1,16 +1,11 @@
 import express from "express";
-import cors from 'cors';
-
+import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 3000;
 
-app.use(cors())
+app.use(cors());
 
-app.post('/api/files', (req, res) => {
-
-})
-
-app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`)
+app.listen(port, () => {
+    console.log(`Server is running in port: ${port}`);
 })
